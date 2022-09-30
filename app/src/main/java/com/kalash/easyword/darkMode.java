72ea4b04b57197964753c2c99252b2472e21a56d
Kalash bharti <112189678+KalashBharti@users.noreply.github.com>
2022-09-30 22:35:26 +0530

    Update darkMode.java

--- app/src/main/java/com/kalash/easyword/darkMode.java
+++ app/src/main/java/com/kalash/easyword/darkMode.java
@@ -1,17 +1,13 @@
 package com.kalash.easyword;
-
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Switch;
-
 import androidx.annotation.BinderThread;
 import androidx.annotation.ColorInt;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
-import com.kalash.easyword.databinding.ActivityMainBinding;
-
+import com.kalash.easyword.databinding.ActivityMainBinding;  //this line give error you have change it according to your android project
 import java.util.ArrayList;
 
 public class darkMode {
@@ -19,12 +15,12 @@ public class darkMode {
     ConstraintLayout layout;
     darkMode(ActivityMainBinding binding)
     {
-        this.binding=binding;
+        this.binding=binding;    // taking Activity main binding inflayter
 
     }
     protected void Active()  //Switch on
     {
-//        binding.background.setBackgroundColor(Color.BLACK);  //background black
+ //background Dark
         binding.background.setBackgroundColor(Color.rgb(0,10,50));
         binding.appName.setTextColor(Color.WHITE);
 
@@ -39,7 +35,7 @@ public class darkMode {
     }
     protected void DeActive()  //Switch Off
     {
-
+            //default mode
           binding.background.setBackgroundColor(Color.WHITE);  //background white
         binding.appName.setTextColor(Color.BLACK);
         binding.inputWord.setBackgroundResource(R.drawable.plaintextbox);
